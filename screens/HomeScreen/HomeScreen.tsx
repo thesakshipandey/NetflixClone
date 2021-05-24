f6ae03ed07c451ad649037d7040bb0bf556f3960
thesakshipandey <sakshi27p@gmail.com>
2021-05-24 13:59:10 +0530

    rendering homescreen flatlist

---
+++ screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Image, FlatList } from 'react-native';
+
+import { Text, View } from '../../components/Themed';
+import styles from './styles';
+import categories from '../../assets/data/categories';
+
+const firstCategory = categories.items[0];
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+        <FlatList
+          horizontal={true}
+          data={firstCategory.movies}
+          renderItem={({item}) => (
+            <Image style={styles.image} source={{ uri: item.poster }}/>
+          )}   
+        />
+    </View>
+  );
+}
+
+export default HomeScreen;
---
+++ screens/HomeScreen/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    
+    image: {
+        width: 100,
+        height: 150,
+        resizeMode: 'cover',
+        borderRadius: 5,
+    },
+});
+
+export default styles;
